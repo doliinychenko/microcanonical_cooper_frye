c1590b4316f4ee83533374957982d10cea0ba215
Dmytro Oliinychenko <doliinychenko@lbl.gov>
2019-03-13 11:15:17 -0700

    Throw error if can't renormalize momenta
    Signed-off-by: Dmytro Oliinychenko <doliinychenko@lbl.gov>

--- microcanonical_sampler.cc
+++ microcanonical_sampler.cc
@@ -666,6 +666,10 @@ void MicrocanonicalSampler::renormalize_momenta(
   QuantumNumbers conserved_final = QuantumNumbers(particles);
   std::cout << "Obtained total momentum: " << conserved_final.momentum
             << std::endl;
+  if (conserved_final.momentum.abs() > required_total_momentum.abs() + 1.e-9) {
+    throw std::runtime_error("Sum of particle masses is larger than "
+      "the required patch energy.");
+  }
 }
 
 void MicrocanonicalSampler::print_rejection_stats() {
